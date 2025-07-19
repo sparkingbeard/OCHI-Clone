3ec83bbd107ed0c8635f50f7c1a38ee4226d41f7
sparkingbeard <tusharbisen8@gmail.com>
2025-07-19 17:36:23 +0530

    eyes component complete

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Navbar from './Components/Navbar'
 import Landingpage from './Components/Landingpage'
 import Marquee from './Components/Marquee'
 import About from './Components/About'
+import Eyes from './Components/Eyes'
 function App() {
   return (
     <div className='w-full min-h-screen bg-zinc-900 text-white '>
@@ -10,6 +11,7 @@ function App() {
       <Landingpage/>
       <Marquee/>
       <About/>
+      <Eyes/>
     </div>
 
   )
---
+++ src/Components/Eyes.jsx
@@ -0,0 +1,45 @@
+import React, { useEffect, useState } from 'react'
+
+function Eyes() {
+
+  const [rotate, setRotate] = useState(0);
+  useEffect(() => {
+    //side effect here
+    window.addEventListener('mousemove', (e)=>{
+      let mouseX = e.clientX;
+      let mouseY = e.clientY;
+
+      let deltaX =   mouseX - window.innerWidth/2;
+      let deltaY =  mouseY - window.innerHeight/2 ;
+
+      var angle = Math.atan2(deltaY, deltaX) * (180/Math.PI); // getting angle in radiance
+
+      setRotate(angle - 180);
+    })
+  });
+  return (
+    <div className='w-full h-screen overflow-hidden'>
+      <div className='bgimg relative bg-cover bg-center bg-[url("https://ochi.design/wp-content/uploads/2022/05/Top-Viewbbcbv-1-scaled.jpg")] w-full h-full'>
+        <div className="eye-container flex gap-10 absolute top-1/2 left-1/2 translate-x-[-50%] translate-y-[-50%]">
+          <div className="eye w-[15vw] h-[15vw] bg-zinc-100 rounded-full flex justify-center items-center">
+            <div className="eyeball relative w-2/3 h-2/3 bg-zinc-950 rounded-full">
+              <div style={{transform: `rotate(${rotate}deg)`}} className="line absolute top-1/2 left-1/2 translate-x-[-50%] translate-y-[-50%] w-full h-10 flex items-center rotate-45">
+                <div className="whiteminiball h-5 w-5 bg-zinc-50 rounded-full"></div>
+              </div>
+
+            </div>
+          </div>
+          <div className="eye w-[15vw] h-[15vw] bg-zinc-100 rounded-full flex justify-center items-center">
+            <div className="eyeball relative w-2/3 h-2/3 bg-zinc-950 rounded-full">
+               <div style={{transform: `rotate(${rotate}deg)`}} className="line absolute top-1/2 left-1/2 translate-x-[-50%] translate-y-[-50%] w-full h-10 flex items-center rotate-45">
+                <div className="whiteminiball h-5 w-5 bg-zinc-50 rounded-full"></div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Eyes
